d2547b6484cb1685be996184e9a3440f97f8d37d
Leydiana Santiago <anitasg@outlook.es>
2020-06-19 15:13:58 -0500

    Estructura

---
+++ SAyCC.AguaPotable/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SAyCC.AguaPotable.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
